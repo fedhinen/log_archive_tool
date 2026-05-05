ccafcd0df2e9f921392fb69581a50e826095e7c2
Victor Ruiz <victor.adrian.ruiz.espindola+dev@gmail.com>
2026-05-05 14:16:49 -0600

    add archive log of executions, clean flags, output dir slash archive

--- log-archive.sh
+++ log-archive.sh
@@ -2,21 +2,30 @@
 
 OPTIND=1
 log_dir="/var/log"
-out_dir=$(pwd)
+out_dir="/var/log"
 
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 LOG_NAME="log_archive_$TIMESTAMP.tar.gz"
 
-while getopts "h?:l:o:" opt; do
+while getopts "hl:o:" opt; do
     case $opt in
-        h|\?)
+        h)
             echo "Usage: $0 [-l log_dir] [-o out_dir]"
             exit 0
             ;;
         l) log_dir="$OPTARG" ;;
         o) out_dir="$OPTARG" ;;
-        \?) echo "Invalid option: -$OPTARG" >&2 ;;
+        \?)
+            echo "Usage: $0 [-l log_dir] [-o out_dir]"
+            exit 1
+            ;;
     esac
 done
 
-tar -zcvf "$out_dir/$LOG_NAME" "$log_dir"
+shift $((OPTIND-1))
+
+mkdir -p "$out_dir/archive"
+
+tar --exclude="$out_dir/archive" -zcvf "$out_dir/archive/$LOG_NAME" "$log_dir"
+
+echo "$TIMESTAMP - $LOG_NAME" >> "$out_dir/archive/log_archive.log"
